8c4a16a63c2af4ec713da5dc654ed5c1be04079f
jtlemkin <jtlemkin@gmail.com>
2019-10-11 16:25:12 -0700

    refactoring

--- scraper.py
+++ scraper.py
@@ -2,19 +2,30 @@ import csv
 import pygit2
 import time
 
+
 def parse_shas(bug_inducing_shas):
-    return bug_inducing_shas.strip('\"').split(",")
+    formatted = bug_inducing_shas
+
+    if bug_inducing_shas[0] == '\"':
+        formatted = formatted.strip('\"')
+    elif bug_inducing_shas[0] == '[':
+        formatted = formatted[1:-1]
+
+    return formatted.split(",")
+
 
 def is_java(fpath):
     return fpath[-4:] == "java"
+
 
 def want_to_skip(line):
     test = line.lstrip()
 
     return not test or test[:2] == "/*" or test[0] == '*' or test[:7] == "package" or test[:6] == "import" or test[0] == "@" or test[:2] == "//"
 
+
 #repo path is the same as the name of the project
-def scrape_buggy(project):
+def scrape_dev_labelled(project):
     repo_path = project
     input_csv_path = project.upper() + ".csv"
     output_csv_path = "bugs.csv"
@@ -63,5 +74,64 @@ def scrape_buggy(project):
                                     writeCSV.writerow([repo_path, date, sha, old_file, line_no, "1"])
 
 
-for project in ["accumulo", "ambari", "hadoop", "jcr", "lucene", "oozie"]:
-    scrape_buggy(project)
+def szz_labeled_task(project, repo, commit, sha, writeCSV):
+    date = time.strftime("%Y-%m-%d", time.gmtime(commit.commit_time))
+
+    try:
+        diff = repo.diff(commit.parents[0], commit, context_lines=0)
+    except IndexError:
+        # This occurs when the blamed commit is the initial commit
+        # We'll have enough data so we'll just skip it
+        return
+
+    for patch in diff:
+        old_file = patch.delta.old_file.path
+
+        if not is_java(old_file):
+            continue
+
+        for hunk in patch.hunks:
+            for line in hunk.lines:
+                if line.origin == '+':
+                    line_no = line.new_lineno
+
+                    # skip comments, import statements, package statements, whitespace only lines, annotations
+                    if want_to_skip(line.content):
+                        continue
+
+                    writeCSV.writerow([project, date, sha, old_file, line_no, "0"])
+
+# This function iterates over the commits in a csv file and does some task on it which can be written to an
+# output csv file
+def scrape(project, inputf, outputf, task):
+    repo_path = project
+    repo = pygit2.Repository(repo_path)
+
+    with open(inputf) as icsvfile:
+        with open(outputf, "a+") as ocsvfile:
+            readCSV = csv.reader(icsvfile, delimiter='\t')
+            writeCSV = csv.writer(ocsvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+            for row in readCSV:
+                bug_inducing_shas = row[1]
+
+                for sha in parse_shas(bug_inducing_shas):
+                    sha = sha.lstrip()
+
+                try:
+                    commit = repo.revparse_single(sha)
+                except Exception:
+                    print("Skipping commit", sha, "in project", project)
+                    continue
+
+                task(project, repo, commit, sha, writeCSV)
+
+def scrape_szz_labelled(project):
+    input_csv_path = project + "/AG_SZZ.txt"
+    output_csv_path = "nonbugs.csv"
+
+    scrape(project, input_csv_path, output_csv_path, szz_labeled_task)
+
+
+for project in ["accumulo", "ambari", "hadoop", "lucene", "oozie"]:
+    scrape_szz_labelled(project)
